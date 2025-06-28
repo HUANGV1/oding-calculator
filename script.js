dd8148b36ae2aef2354bd029f30c8667fb0beb5b
HUANGV1 <viho2007@gmail.com>
2025-06-28 00:14:01 -0400

    created basic calculator layout

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
 
-function add(a, b) {
+let firstNum;
+let secondNum;
+let operation;
+
+function addition(a, b) {
     return a+b;
 }
 
-function subtract(a, b) {
+function subtraction(a, b) {
     return a-b;
 }
 
-function multiply(a, b) {
+function multiplication(a, b) {
     return a*b;
 }
 
-function divide(a, b) {
+function division(a, b) {
     if (b==0) {
         return 'CANNOT DIVIDE BY ZERO';
-    }
-    else {
+    } else {
         return a/b;
     }
+}
+
+function operate(a, b, op) {
+    if (op=='+') {
+        return addition(a, b);
+    } else if (op=='-') {
+        return subtraction(a, b);
+    } else if (op=='*') {
+        return multiplication(a, b);
+    } else if (op=='/') {
+        return division(a, b);
+    } else {
+        return 'OPERATOR ISSUE';
+    }
+
 }
