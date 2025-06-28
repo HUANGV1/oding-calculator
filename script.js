84d75836dd1617d15e6cef89b4076e34ce6ff12e
HUANGV1 <viho2007@gmail.com>
2025-06-28 01:38:43 -0400

    created functions, calculator works basic

--- script.js
+++ script.js
@@ -1,7 +1,52 @@
 
-let firstNum;
-let secondNum;
-let operation;
+let firstNum='';
+let secondNum='';
+let operator='';
+let answer='';
+const display=document.querySelector('#display');
+const digitButtonPressed=document.querySelectorAll('#buttons-numbers button');
+const operatorPressed=document.querySelectorAll('#buttons-operators button');
+const equalPressed=document.querySelector('#button-equals');
+const clearPressed=document.querySelector('#button-clear');
+
+
+digitButtonPressed.forEach(button =>{
+    button.addEventListener('click', ()=>{
+        if (operator==''){
+            firstNum+=button.textContent;
+            display.textContent=firstNum;
+            console.log('firstNum'+firstNum);
+        } else {
+            secondNum+=button.textContent;
+            display.textContent=secondNum;
+            console.log('secondNum'+secondNum);
+        }
+        
+    });
+});
+
+
+operatorPressed.forEach(button =>{
+    button.addEventListener('click', ()=>{
+        operator=button.textContent;
+        console.log('operator'+operator);
+    });
+});
+
+
+equalPressed.addEventListener('click', ()=>{
+    answer=operate(+firstNum, +secondNum, operator);
+    display.textContent=Math.round(answer*100)/100;
+    console.log('answer'+answer);
+});
+
+clearPressed.addEventListener('click', ()=>{
+    firstNum='';
+    secondNum='';
+    operator='';
+    display.textContent='0';
+});
+
 
 function addition(a, b) {
     return a+b;
@@ -28,12 +73,13 @@ function operate(a, b, op) {
         return addition(a, b);
     } else if (op=='-') {
         return subtraction(a, b);
-    } else if (op=='*') {
+    } else if (op=='x') {
         return multiplication(a, b);
-    } else if (op=='/') {
+    } else if (op=='÷') {
         return division(a, b);
     } else {
         return 'OPERATOR ISSUE';
     }
 
 }
+
